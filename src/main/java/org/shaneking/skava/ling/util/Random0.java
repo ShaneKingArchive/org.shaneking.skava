0eb5960f2246c308f74dfb4391749956ef083ab5
ShaneKing <github@azolla.org>
2018-07-18 22:38:16 +0800

    0.0.4

---
+++ src/main/java/org/shaneking/skava/ling/util/Random0.java
@@ -0,0 +1,19 @@
+/*
+ * @(#)Random0.java		Created at 2018/7/18
+ *
+ * Copyright (c) ShaneKing All rights reserved.
+ * ShaneKing PROPRIETARY/CONFIDENTIAL. Use is subject to license terms.
+ */
+package org.shaneking.skava.ling.util;
+
+import java.security.SecureRandom;
+
+public class Random0
+{
+  private static final SecureRandom SR = new SecureRandom();
+
+  public static int nextRangeInt(int max)
+  {
+    return Math.abs(SR.nextInt()) % max;
+  }
+}
